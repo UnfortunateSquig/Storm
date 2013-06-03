5ac54ab214b3c7d5686f2e26cf6de04404d11623
Larry Garrett <lgarrettiii@gmail.com>
2013-06-03 19:31:20 -0400

    Updated camera behavior to prototype stage.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,13 @@ namespace Assets.Scripts
 {
     class CameraController : MonoBehaviour
     {
-        private GameObject _target;
+        public GameObject Target;
         private GameObject _camera;
 
         public float AcceptableXDifference = 1.0f; // How far is the target allowed to move away from the center of the camera before the camera tries to catch up?
         public float AcceptableYDifference = 5.0f;
+        public float DefaultCameraRest; // In comparison to the player object!
+        public float CameraSinkY = 3.0f; // How far below the character model is the camera comfy?
         
         public void Start()
         {
@@ -17,9 +19,22 @@ namespace Assets.Scripts
 
         public void Update()
         {
-            if (transform.position.x - _target.transform.position.x > AcceptableXDifference)
+            DefaultCameraRest = Target.transform.position.y - CameraSinkY;
+
+            if (transform.position.y - Target.transform.position.y >= AcceptableYDifference) // When the player is too far 
             {
-                return;
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, DefaultCameraRest, Time.deltaTime *2), transform.position.z);
+            }
+
+            if (transform.position.x - Target.transform.position.x >= AcceptableXDifference) // When the player is to the left of the camera, move left.
+            {
+                transform.position = new Vector3(Mathf.Lerp(transform.position.x, Target.transform.position.x, Time.deltaTime), transform.position.y, transform.position.z);
+            }
+
+            if (-(transform.position.x - Target.transform.position.x) >= AcceptableXDifference) // When the player is to the left of the camera, move right.
+            {
+                Debug.Log(Target.transform.position.x - transform.position.x);
+                transform.position = new Vector3(Mathf.Lerp(transform.position.x, Target.transform.position.x, Time.deltaTime), transform.position.y, transform.position.z);
             }
         }
     }
